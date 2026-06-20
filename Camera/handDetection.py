6f3cb8be8a5e214e009c28f7b5e871a5e2a70b14
Breanna Chin <breanna.a.chin@gmail.com>
2026-06-20 12:13:56 -0400

    added more video preprocessing

--- Camera/handDetection.py
+++ Camera/handDetection.py
@@ -2,9 +2,11 @@ import cv2
 
 def process_frame(frame):
 
+        #Edge detection
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        edges = cv2.Canny(frame_rgb, 100, 700)
-        edges_bgr = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+        #Gaussian Bluring
+        blurred = cv2.GaussianBlur(frame_rgb, (5, 5), 0)
+        edges = cv2.Canny(blurred, 100, 700)
         #looks for strong changes in brightness and color and outlines hte boundries
         #black images, white edges
 
@@ -12,6 +14,18 @@ def process_frame(frame):
         #700 - upper threshold
         #change after testing
 
-        return edges_bgr
+        # Contours
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #within the white edges finds shapes
+
+        output = frame.copy()
+
+        if contours:
+                largest_contour = max(contours, key=cv2.contourArea)
+                cv2.drawContours(output, [largest_contour], -1, (0, 255, 0), 2)
+            #find the largest one and draw it on the output
+
+
+        return output
 
 
